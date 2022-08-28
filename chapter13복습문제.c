225edc35b7710b6e4d141de161e70ddd22c79575
HBSTUDIO <hbstudio@naver.com>
2022-08-28 22:10:44 +0900

    13장 프로그래밍 연습 2번 문제 진행 중

--- chapter13복습문제.c
+++ chapter13복습문제.c
@@ -25,9 +25,31 @@ int main(int argc, char *argv[]) {
 	////기본적인 파일 입출력의 원리만 알면 쉬게 해결 할 수 있다.
 
 	//Q2. 소스파일, 타깃파일 명령행으로 받아 파일 복사 프로그램 작성
-	//argv[1]이 타깃, argv[2]가 소스파일로
+	//argv[1]이 소스, argv[2]가 타겟파일로
+	//바이너리 모드로..
+	char ch;
+	FILE* fsrc, * ftrg;	//source파일, target파일
 
+	if (2 >= argc) { //명령행이 없다면...
+		puts("명령행 전달 인자가 없거나 부족합니다.");
+		exit(EXIT_FAILURE);
+	}
 
+	if ((fsrc = fopen(argv[1], "rb")) == NULL) {
+		puts("source 파일이 없습니다.");
+		exit(EXIT_FAILURE);
+	}
+
+	if ((ftrg = fopen(argv[2], "wb")) == NULL) {	//필요 없을듯..
+		puts("target 파일이 없습니다.");
+		exit(EXIT_FAILURE);
+	}
+
+	fread(&ch, sizeof(char), 1, fsrc);	//1byte 단위로 읽어
+	fwrite(&ch, sizeof(char), 1, ftrg);	//1byte 단위로 썼다
+
+	fclose(fsrc);
+	fclose(ftrg);
 
 	return 0;
 }
