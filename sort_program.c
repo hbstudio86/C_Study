6847e33b189e7c1c1f2e0e6e1538761ca41a1079
HBSTUDIO <hbstudio@naver.com>
2022-12-30 17:14:37 +0900

    Select Bubble SORT구현완료

--- sort_program.c
+++ sort_program.c
@@ -11,6 +11,11 @@
 //###함수 선언###
 int _fCheck(int*);
 void _fResult(int[], int[], int);	//정렬후 값 출력용
+void _fResult2(int[], int);
+void _fResult3(int[], int, int, int);	//확인용3
+void _fSwap(int*, int*,int,int);			//SWAP
+void _fOutComp(int);
+void _fOutComp2(int);	//결과 출력용
 void _fSelectSORT(int[], int);	//선택 정렬
 void _fBubbleSORT(int[], int);	//버블 정렬
 
@@ -63,9 +68,12 @@ int main(void) {
 		switch (_iCommand)
 		{
 		case 1:
+			puts("Before SORT");
+			_fResult2(_iParr2, _iArrSize);
 			_fSelectSORT(_iParr2, _iArrSize);
 			break;
 		case 2:
+			_fResult2(_iParr2, _iArrSize);
 			_fBubbleSORT(_iParr2, _iArrSize);
 			break;
 		default:
@@ -94,18 +102,56 @@ int _fCheck(int* input) {
 	return *input;
 }
 
-//확인용:정렬 전/후 비교
+//확인용1:정렬 전/후 비교
 void _fResult(int bf[], int af[], int n) {
 	for (int i = 0; i < n; i++) {
-		printf("[%02d] %06d | %06d \n", i, bf[i], af[i]);
+		printf("[%02d] %6d | %6d \n", i, bf[i], af[i]);
+	}
+}
+//확인용2:배열 출력용
+void _fResult2(int ar[], int n) {
+	for (int i = 0; i < n; i++) {
+		printf("[%02d] %6d \n", i, ar[i]);
+	}
+}
+//확인용3:시각화
+void _fResult3(int ar[], int n, int pnt1, int pnt2) {
+	char Edge = ' ';
+	for (int i = 0; i < n; i++) {
+		if (i == pnt1)
+			Edge = '<';
+		else if (i == pnt2)
+			Edge = '>';
+		else
+			Edge = ' ';
+		printf("[%02d] %6d %c \n", i, ar[i], Edge);
 	}
 }
 
+//교환용
+void _fSwap(int* src1, int* src2,int bn, int an) {
+	puts("!SWAP!");
+	printf("[%d]<---->[%d]\n", bn, an);
+	int tmp = *src2;
+	*src2 = *src1;
+	*src1 = tmp;
+}
+
+//내용 출력용: 교환된 내용을 출력 한다
+void _fOutComp(int comp) {
+	printf("Select SORT Result: Compare_%d\n", comp);
+}
+
+//내용 출력용2: 모든 결과를 출력 한다
+void _fOutComp2(int comp) {
+	puts("===== Result =====");
+	printf("*Result: Total Swap = %d\n", comp);
+	puts("=====  END   =====");
+}
 //선택정렬
 void _fSelectSORT(int src[],  int n) {
 	puts("Select SORT Start");
 	int Step = 0, Comp = 0; //step수와 비교 횟수를 표시
-	//int MinVal = src[0];		//최소값 저장, 배열 첫 멤버를 저장 함
 	int MinVal;					//index 번호를 저장
 	int tmp = 0;	//임시 변수, 최소값 교환을 위해 사용
 	for (int i = 0; i < n; i++) {
@@ -114,16 +160,31 @@ void _fSelectSORT(int src[],  int n) {
 			if (src[MinVal] > src[j]) {
 				MinVal = j;	//최소값이면 그값의 index번호를 저장
 			}
-			Comp++;
 		}
 		//1회전이 끝나면..교환이 이루어 진다.
-		tmp = src[i];
-		src[i] = src[MinVal];
-		src[MinVal] = tmp;
+		if (src[i] != src[MinVal]) {	//교환이 이루어 졌을 때만 comp와 swap이 이루어 진다.
+			_fSwap(&src[i], &src[MinVal], i, MinVal);
+			Comp++;	//swap cnt를 따로 빼야 한다????? 왜냐 이건 비교 카운트라 그렇다
+		}
+		_fOutComp(Comp);
+		_fResult3(src, n, i, MinVal);
 	}
-	printf("Select SORT Result: Compare_%d\n", Comp);
+	_fOutComp2(Comp);	//총 결과값
 }
 
 void _fBubbleSORT(int src[], int n) {
-
+	puts("Select SORT Start");
+	int Step = 0, Comp = 0;
+	int tmp = 0;
+	for (int i = 1; i < n; i++) {
+		for (int j = 0; j < (n-i); j++) {
+			if (src[j] > src[j + 1]) {
+				_fSwap(&src[j + 1], &src[j], j + 1, j);
+				Comp++;
+				_fOutComp(Comp);
+				_fResult3(src, n, j + 1, j);
+			}
+		}
+	}
+	_fOutComp2(Comp);
 }
