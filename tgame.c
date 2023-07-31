81ad44d926f27062c346457dd62c322ad1beb0bd
HBSTUDIO <hbstudio@naver.com>
2023-07-31 23:02:22 +0900

    TEST

--- tgame.c
+++ tgame.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
-//#include <Windows.h>
+#include <Windows.h>
 #include <stdlib.h>
+
+#define PAUSE system("pause")
+
 
 int main() {
 
-	system("pause");
+	int j = 0;
+
+	for (int i = 1; i < 16; i++) {
+		j = 16 * i+8;
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),j);
+		printf("     ¡á¡á¡á¡á     \t\t %X\n", j);
+		//PAUSE;
+	}
 
 	return 0;
 }
