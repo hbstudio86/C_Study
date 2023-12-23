74af896d69b5237c4eaddbf40a26edc792ebd2ce
hbstudio <hbstudio@naver.com>
2023-12-23 23:59:29 +0900

    자료구조 연습

--- DataStructure.c
+++ DataStructure.c
@@ -1,5 +1,9 @@
+#pragma warning(disable:4996)
 #include <stdio.h>
 #include <stdlib.h>
+
+//#define DEBUG 1
+
 
 /*배열 (기본)
 1. 배열 생성
@@ -7,8 +11,8 @@
 3. 데이터 삭제
 
 */
-void arrCreate(int);	// 배열 생성 : 동적으로..
-void arrAdd(int);		// 데이터 삽입
+int * arrCreate(int);	// 배열 생성 : 동적으로..
+void arrAdd(int [], int*, int);		// 데이터 삽입
 void arrDel(int);		// 데이터 삭제
 /*배열 (링)
 1. 배열 생성
@@ -28,6 +32,34 @@ int main(void) {
 
 	arrPrint(arr1, 10);
 
+		//배열을 새롭게 정의
+	int* pArray = NULL;
+	int szElement;
+	int _ifirst = -1;	// 현재 배열의 위치
+	
+	printf(" ! Information : 생성할 배열의 원소 갯수를 입력하세요:_____\b\b\b\b\b");
+	scanf("%d", &szElement);
+
+	pArray = arrCreate(szElement);
+
+	if (pArray != NULL) {	// 새롭게 배열이 생성되었다면...
+		free(pArray);
+		printf(" ! Information : 생성된 배열이 삭제 되었습니다.\n");
+#ifdef DEBUG
+		printf("_?_DEBUG_FREE_ARRAY_ADDRESS_0x%p\n", pArray);
+#endif // DEBUG
+
+	}
+	else {	// 배열 생성이 되지 않았다면...
+		printf(" ! Information : Array is not Created.n");
+	}
+
+	while (1) {
+
+
+	}
+
+
 	// 연결 리스트
 
 	// 스택
@@ -39,11 +71,37 @@ int main(void) {
 	return 0;
 }
 
-void arrCreate(int n) {	// 배열 생성 : 동적으로..
-
+int* arrCreate(int szArray) {	// 배열 생성 : 동적으로..	배열 주소 리턴
+	if (szArray > 0) {	// 0이상의 갯수가 입력 되었을 때만 
+		int* pArr = (int*)malloc(sizeof(int) * szArray);	//입력한 수대로 배열 생성
+		printf(" ! Information : %d 크기의 배열이 생성 되었습니다.\n", szArray);
+#ifdef DEBUG
+		printf("_?_DEBUG_ARRAY_ADDRESS_0x%p\n", pArr);
+#endif // DEBUG
+		return pArr;	// 배열 주소 리턴
+	}
+	else {
+		printf(" ! Warning : 배열의 크기는 0개 이하 일 수 없습니다.\n");
+		return NULL;	// 아무것도 반환 하지 않음
+	}
 
 }
-void arrAdd(int n) {		// 데이터 삽입
+void arrAdd(int arr[], int * first, int sz_element) {		// 데이터 삽입 : 배열의 주소와 시작 위치를 받음
+#ifdef DEBUG
+	printf("_?_DEBUG_ARRAY_ADDRESS_0x%p_FIRST_%d\n", arr, *first);
+#endif // DEBUG
+	// first를 어떤 식으로 처리 해야 하는지...
+	if (*first < sz_element - 1) {	// sz_element 수보다 작아야 삽입 할 수 있게 한다. 
+		*first += 1;				// index를 하나 옮김
+		printf(" ! Information : 배열에 넣을 값을 입력하세요._____\b\b\b\b\b");
+		scanf("%d", &arr[*first]);
+#ifdef DEBUG
+		printf("_?_DEBUG_INPUT_ARRAY_ADDRESS_0x%p_VALUE_%d\n", &arr[*first], arr[*first]);
+#endif // DEBUG
+	}
+	else {
+		printf(" ! Warning : 배열이 만원상태 입니다.\n");
+	}
 
 }
 void arrDel(int n) {		// 데이터 삭제
@@ -53,7 +111,7 @@ void arrDel(int n) {		// 데이터 삭제
 void arrPrint(int arr[], int n) {
 
 	for (int i = 0; i < n; i++) {
-		printf("Array value is %d \n", arr[i]);
+		printf("Array value is %d _ index [%d] \n", arr[i],i);
 	}
 
 }
